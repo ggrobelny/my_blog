e195fc479b1469d4204712b6c710903d7b4685c2
ggrobelny <ggrobelny@outlook.fr>
2022-02-06 21:20:30 +0100

    Utworzenie widoków listy i szczegółów, Dodanie wzorców adresów URL do widoków, Kanoniczne adresy URL dla modeli, Utworzenie szablonów dla widoków

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog.views import post_detail, post_list
+
+app_name = 'blog'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
+
