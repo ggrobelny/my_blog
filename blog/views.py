57bb2e06af060dd49ff96d890cdf4d7b086dd318
ggrobelny <ggrobelny@outlook.fr>
2022-02-06 21:42:41 +0100

    paginacja

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request,'blog/post/list.html', {'posts': posts})
+    # posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # Trzy posty na każdej stronie.
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+    # Jeżeli zmienna page nie jest liczbą całkowitą,
+    # wówczas pobierana jest pierwsza strona wyników.
+        posts = paginator.page(1)
+    except EmptyPage:
+    # Jeżeli zmienna page ma wartość większą niż numer ostatniej strony
+    # wyników, wtedy pobierana jest ostatnia strona wyników.
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
