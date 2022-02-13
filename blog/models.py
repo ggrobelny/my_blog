b1668cbafbd6a56b9729a1dde8d4947b398a5bbc
ggrobelny <ggrobelny@outlook.fr>
2022-02-13 20:47:31 +0100

    utworzenie systemu komentyrzy, models.py class Comment

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,
@@ -39,5 +40,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Komentarz dodany przez {} dla posta {}'.format(self.name, self.post)
 
 
