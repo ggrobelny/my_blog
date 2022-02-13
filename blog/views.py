0aeb2011fe6494eb4e6cf92efed47d1feb8d141b
ggrobelny <ggrobelny@outlook.fr>
2022-02-13 17:13:18 +0100

    obsluga formularzy na widokach

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 def post_list(request):
     # posts = Post.published.all()
@@ -32,3 +33,18 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+# Pobranie posta na podstawie jego identyfikatora.
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+# Formularz został wysłany.
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+# Weryfikacja pól formularza zakończyła się powodzeniem...
+            cd = form.cleaned_data
+# ... więc można wysłać wiadomość e-mail.
+    else:
+        form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post,'form': form})
